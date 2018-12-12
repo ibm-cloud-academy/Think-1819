172f2475cb88f8a86e5ccdf97b4a2ed7e641105d
Budi Darmawan <vbudi@us.ibm.com>
2018-12-12 13:27:01 -0600

    fixed test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,10 +16,24 @@ else
   echo "build.properties : not found"
 fi 
 
+DEP_NAME="catalog-deployment"
+SVC_NAME="catalog-service"
+appl=`kubectl get deployment --namespace ${CLUSTER_NAMESPACE} | grep "${DEP_NAME}" | wc -l`
+
+
 APP_NAME=$(kubectl get pods --namespace ${CLUSTER_NAMESPACE} -o json | jq -r '.items[].status | select(.containerStatuses!=null) | .containerStatuses[] | select(.image=="'"${IMAGE_REPOSITORY}:${IMAGE_TAG}"'") | .name' | head -n 1)
-APP_SERVICE=$(kubectl get services --namespace ${CLUSTER_NAMESPACE} -o json | jq -r ' .items[] | select (.spec.selector.app=="'"${APP_NAME}"'") | .metadata.name ')
+if ((appl==1)); then
+  APP_SERVICE=${SVC_NAME}
+else
+  APP_SERVICE=test-${SVC_NAME}
+fi
+
+echo "Testing ${APP_NAME} with ${APP_SERVICE}"
+
 IP_ADDR=$( ibmcloud cs workers ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep normal | head -n 1 | awk '{ print $2 }')
 PORT=$( kubectl get services --namespace ${CLUSTER_NAMESPACE} | grep ${APP_SERVICE} | sed 's/.*:\([0-9]*\).*/\1/g' )
+
+echo "Testing http://${IP_ADDR}:${PORT}/items"
 
 curl "http://${IP_ADDR}:${PORT}/items"
 
